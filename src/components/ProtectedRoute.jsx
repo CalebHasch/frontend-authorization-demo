9cde83456f9e5fd454ad4eff5b2eee0774bb87ae
Caleb <chasch96@gmail.com>
2024-08-06 19:50:33 +0800

    created register route

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, useLocation } from "react-router-dom";
+
+function ProtectedRoute({ isLoggedIn, children, anonymous = false }) {
+  const location = useLocation();
+  const from = location.state?.from || "/";
+  if (anonymous && isLoggedIn) {
+    return <Navigate to={from} />;
+  }
+  if (!isLoggedIn && !anonymous) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
